9d142ba0b92a5c08ac1eb745193f9552e884fc2a
Muhamad Rasyid Ridho <70908423+acid1010@users.noreply.github.com>
2025-04-21 21:58:32 +0700

    Update simulasiPenjualan.js

--- src/simulasiPenjualan.js
+++ src/simulasiPenjualan.js
@@ -24,9 +24,9 @@ const SimulasiPenjualan = () => {
   const netProfitBulanan = labaBulanan - opBulanan;
 
   const sharingDivisiHarian = netProfitHarian * 0.6;
-  const sharingPendanaHarian = netProfitHarian * 0.4;
+  const sharingPendanaHarian = investasi / 11450000 * netProfitHarian * 0.4;
   const sharingDivisiBulanan = netProfitBulanan * 0.6;
-  const sharingPendanaBulanan = netProfitBulanan * 0.4;
+  const sharingPendanaBulanan = investasi / 11450000 * netProfitBulanan * 0.4;
 
   const totalReturn6Bulan = sharingPendanaBulanan * 6;
   const totalReturn12Bulan = sharingPendanaBulanan * 12;
